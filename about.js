d45a5e5da16b6b593e8f90bedd7bd1c89d0eda77
Pruthvi-Parate <parpru4228@gmail.com>
2022-07-01 14:45:12 +0530

    first commit

---
+++ about.js
@@ -0,0 +1,45 @@
+window.addEventListener("scroll", progressAll);
+
+function progressAll() {
+  const triggerProgress = (window.innerHeight / 8) * 4;
+  const progressTop = document.getElementById("tech");
+
+  const progressStart = progressTop.getBoundingClientRect().top;
+
+  if (progressStart < triggerProgress) {
+    // HTML
+
+    const progress = document.getElementById("htmldone");
+    progress.style.width = '95%';
+
+    // CSS
+
+    const progressCss = document.getElementById("cssdone");
+    progressCss.style.width = progressCss.getAttribute("data-done");
+    progressCss.style.opacity = 1;
+
+    //SASS
+
+    const progressSass = document.getElementById("sassdone");
+    progressSass.style.width = progressSass.getAttribute("data-done");
+    progressSass.style.opacity = 1;
+
+    // BOOTSTRAP
+
+    const progressBootstrap = document.getElementById("bootdone");
+    progressBootstrap.style.width = progressBootstrap.getAttribute("data-done");
+    progressBootstrap.style.opacity = 1;
+
+    // JAVASCRIPT
+
+    const progressJavscript = document.getElementById("jsdone");
+    progressJavscript.style.width = progressJavscript.getAttribute("data-done");
+    progressJavscript.style.opacity = 1;
+
+    // REACT
+
+    const progressReact = document.getElementById("reactdone");
+    progressReact.style.width = progressReact.getAttribute("data-done");
+    progressReact.style.opacity = 1;
+  }
+}
